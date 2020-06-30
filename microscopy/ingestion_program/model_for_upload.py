a002de3beddd9cee35bf282ded5af1f5b1da8c8c
Herilalaina Rakotoarison <rkt.herilalaina@gmail.com>
2020-06-30 11:18:35 +0200

    add microscopy

---
+++ microscopy/ingestion_program/model_for_upload.py
@@ -0,0 +1,46 @@
+"""This file is for patches classification (step 1).
+
+Sample predictive model.
+You must supply at least 4 methods:
+- fit: trains the model.
+- predict: uses the model to perform predictions.
+"""
+from sklearn.base import BaseEstimator
+from sklearn.ensemble import RandomForestClassifier
+
+class Model(BaseEstimator):
+    """Main class for Classification problem."""
+
+    def __init__(self):
+        """Init method.
+
+        We define here a simple (shallow) CNN.
+        """
+        self.is_trained = False
+
+        self.model = RandomForestClassifier(random_state=42)
+
+    def fit(self, X, y):
+        """Fit method.
+
+        This function should train the model parameters.
+        Args:
+            X: Training data matrix of dim num_train_samples * num_feat.
+               An image has the following shape 4800 features
+               (it can be reshape to (40, 40, 3)).
+            y: Training label matrix of dim num_train_samples.
+        Both inputs are numpy arrays.
+        """
+        self.model.fit(X / 255, y)
+        self.is_trained = True
+
+    def predict(self, X):
+        """Predict method.
+
+        Args:
+            X: Training data matrix of dim num_train_samples * num_feat.
+               An image has the following shape 4800 features
+               (it can be reshape to (40, 40, 3)).
+        This function should provide *probabilities* of labels on (test) data.
+        """
+        return self.model.predict_proba(X / 255)[:, 0]
